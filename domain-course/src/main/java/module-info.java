bf42076f3bbb43b2ddec5c2688787cc1ba1a2f57
WW <streetpoet@163.com>
2021-12-28 12:45:15 -0600

    remove module system

--- domain-course/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module domain.course {
-    requires lombok;
-    requires org.slf4j;
-//    exports com.spstudio.zheng.domain.model;
-//    exports com.spstudio.zheng.domain.port.incoming;
-//    exports com.spstudio.zheng.domain.port.outgoing;
-//    exports com.spstudio.zheng.domain.service;
-}
